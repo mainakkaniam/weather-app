5b0eca362ef6c3d68624e704816a25d7086b1078
mainak <heellomeelo@gmail.com>
2023-02-12 21:14:45 +0530

    cant use same api more than once

--- weather.js
+++ weather.js
@@ -1,4 +1,4 @@
-keys = '45c121aef188d2d6b2042dce4ce1381a';
+keys = '0c4d22a8ee16c1989a17fdf45452ddc9';
 async function search()
 {
     const phrase=document.querySelector('input[type="text"]').value;
@@ -20,5 +20,35 @@ async function search()
 const debouncedSearch= _.debounce(()=>{
     search();
 },600);
+async function showWeather(lat,lon,name)
+{
+    const response=await fetch('https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${keys}&units=metric');
+    const data=await response.json();
+    console.log(data);
+    const temp=data.main.temp;
+    const feelslike=data.main.feels_like;
+    const humidity=data.main.humidity;
+    const wind=data.wind.speed;
+    const icon=data.weather[0].icon;
+    document.getElementById('city').innerHTML=name;
+    document.getElementById('degrees').innerHTML=temp+'&#8451';
+    document.getElementById('feelsLikeValue').innerHTML=feeslLike+'&#8451';
+    document.getElementById('windValue').innerHTML=wind+'km/h';
+    document.getElementById('humidityValue').innerHTML=humidity+'%';
+    document.getElementById('icon').src='http://openweathermap.org/img/wn/${icon}@4x.png';
+    document.querySelector('form').style.display='none';
+    document.getElementById('weather').style.display='block';
+
+}
 document.querySelector('input[type="text"]')
 .addEventListener( 'keyup', debouncedSearch);
+document.body.addEventListener('click',ev=>
+{
+    const li=ev.target;
+    const {lat,lon,name}=li.dataset;
+    if(!lat)
+    {
+        return;
+    }
+    showWeather(lat,lon,name);
+})
