09e245b1a9e8ebe05768504a783a377b96271eae
mainak <heellomeelo@gmail.com>
2023-02-12 18:23:55 +0530

    search bar connected to api

--- weather.js
+++ weather.js
@@ -2,9 +2,20 @@ keys = '45c121aef188d2d6b2042dce4ce1381a';
 async function search()
 {
     const phrase=document.querySelector('input[type="text"]').value;
-    const response =await fetch( `https://api.openweathermap.org/data/2.5/weather?q=${phrase}&appid=${keys}`);
+    const response =await fetch( `http://api.openweathermap.org/geo/1.0/direct?q=${phrase}&limit=5&appid=${keys}`);
     const data=await response.json();
+    const ul=document.querySelector('form ul');
     console.log(data);
+    ul.innerHTML='';
+    for(let i=0;i<data.length;i++)
+    {
+        const {name,lat,lon,country}=data[i];
+        ul.innerHTML+=`<li 
+         data-lat="${lat}"
+         data-lon="${lon}" 
+         data-name="${name}">
+         ${name} <span>${country}</span></li>`;
+    }
 }
 const debouncedSearch= _.debounce(()=>{
     search();
