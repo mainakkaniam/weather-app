5fbc824b906539f91aa58143bad055235146c3bd
mainak <heellomeelo@gmail.com>
2023-02-12 10:51:56 +0530

    waiting for key to be active

--- weather.js
+++ weather.js
@@ -1,7 +1,13 @@
-const key="84715a32d97da81b61e63583133c8cfb";
-document.querySelector('input[type="text"]')
-.addEventListener( 'keyup', ev=>
+keys = '45c121aef188d2d6b2042dce4ce1381a';
+async function search()
 {
-    const phrase =ev.target.value;
-    console.log(phrase);
-});
+    const phrase=document.querySelector('input[type="text"]').value;
+    const response =await fetch( `https://api.openweathermap.org/data/2.5/weather?q=${phrase}&appid=${keys}`);
+    const data=await response.json();
+    console.log(data);
+}
+const debouncedSearch= _.debounce(()=>{
+    search();
+},600);
+document.querySelector('input[type="text"]')
+.addEventListener( 'keyup', debouncedSearch);
